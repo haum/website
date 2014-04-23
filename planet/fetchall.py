37964230b7e42856208fc3dbaf93b15692dfc25c
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2014-04-23 23:33:19 +0200

    Ajout d'un planet auto-généré

---
+++ planet/fetchall.py
@@ -0,0 +1,72 @@
+#! /usr/bin/env python
+# -*- coding:utf8 -*-
+#
+# fetchall.py
+#
+# Copyright © 2014 Mathieu Gaborit (matael) <mathieu@matael.org>
+#
+#
+# Distributed under WTFPL terms
+#
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#                    Version 2, December 2004
+#
+# Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+#
+# Everyone is permitted to copy and distribute verbatim or modified
+# copies of this license document, and changing it is allowed as long
+# as the name is changed.
+#
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+#
+#  0. You just DO WHAT THE FUCK YOU WANT TO.
+#
+
+"""
+
+"""
+
+from glob import glob
+from collections import namedtuple
+
+OUTPUT_FILE = '../planet.rst'
+
+HEADER = """
+Planet
+======
+
+Voilà ceux qui parlent de nous sur leurs blogs
+
+"""
+
+def output(f_res):
+    # f_res : fetch results
+
+    with open(OUTPUT_FILE, 'w') as out:
+        out.write(HEADER)
+
+        for r in f_res:
+            out.write('`'+r[0]+'`__ par '+r[2]+'\n')
+            out.write('-'*(9+len(r[0]+r[2]))+'\n\n')
+            out.write('__ '+r[1]+'\n\n')
+
+
+def main():
+    # récupère la liste des scripts source
+    # et lance la fonction fetch()
+    # puis met en forme
+
+    fetch_results = []
+
+    for i in glob('sources/*.py'):
+        if i!='sources/__init__.py':
+            a = __import__(i.replace('/','.').replace('.py',''))
+            fetch_results += a.__getattribute__(i.replace('sources/','').replace('.py','')).fetch()
+
+    output(fetch_results)
+
+
+
+
+if __name__=='__main__':main()
