40bbce1d3fd2f348bd078a8f98c17110befe8cdb
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2014-08-14 17:20:05 +0200

    Changement de style

---
+++ _static/calendar.js
@@ -0,0 +1,40 @@
+/*
+ * calendar.js
+ *
+ * Copyright (C) 2014 Mathieu Gaborit (matael) <mathieu@matael.org>
+ *
+ *
+ * Distributed under WTFPL terms
+ *
+ *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *                    Version 2, December 2004
+ *
+ * Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+ *
+ * Everyone is permitted to copy and distribute verbatim or modified
+ * copies of this license document, and changing it is allowed as long
+ * as the name is changed.
+ *
+ *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ *  0. You just DO WHAT THE FUCK YOU WANT TO.
+ *
+ */
+
+/***** Template :
+    {'date': ['JJ', 'mois', 'AAAA', 'HH', 'MM'],
+     'lieu': 'Somewhere over the rainbow',
+     'texte': 'Lorem ipsum dolor sit amet sed consecuenturLorem ipsum dolor sit amet sed consecuenturLorem ipsum dolor sit amet sed consecuenturLorem ipsum dolor sit amet sed consecuentur'},
+ */
+
+/*** PLEASE USE SHORT MONTH NAMES ***/
+var events = [
+    {'date': ['03', 'sept', '2014', '18', '30'],
+     'lieu': '#opendatapero au Venezzia - Pl. de la République',
+     'texte': 'Retrouvons nous en terrasse pour discuter opendata tous ensemble !'},
+    {'date': ['19', 'sept', '2014', '18', '00'],
+     'lieu': 'Hackathon Opendata - Ruche Numérique',
+     'texte': "Hackathon autour de l'ouverture des données en partenariat avec le Conseil Général et la Ruche Numérique <a href='/hackathon_opendata.html'>En Savoir plus >></a>"},
+];
+
