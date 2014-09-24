81dd2e5d2bbfc14017f2fe9455ef476909131927
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2014-09-25 01:35:45 +0200

    New global for planet integration

--- pelicanconf.py
+++ pelicanconf.py
@@ -38,3 +38,8 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# planet regeneration
+
+from planet.fetchall import fetch
+PLANET = fetch('planet/sources')
