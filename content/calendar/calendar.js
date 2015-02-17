8f2fc251744f04c5cc1be3d38a9e97cd3b83f601
jblb <jerome@jblb.net>
2015-02-17 22:29:15 +0100

    do not track calendar js

--- content/calendar/calendar.js
+++
@@ -1,6 +0,0 @@
-
-
-/*** PLEASE USE SHORT MONTH NAMES ***/
-var past_events = [];
-var future_events = [{"date": ["14", "oct.", "2014", "18", "30"], "texte": " \"Fin de la nouvelle formation arduino (niveau 1)\"", "lieu": "Arduino 3.3 - La Ruche Num\u00e9rique"}, {"date": ["15", "oct.", "2014", "19", "00"], "texte": " 2\u00e8me \u00e9dition de la conf\u00e9rence aux sujets libres. Venez parler de ce qui vous passionne ! <a href=\"http://talks.haum.org/b/4\">Renseignements et inscriptions >></a>", "lieu": "Conf\u00e9rence Libre #2 - B.U. Universit\u00e9 du Maine"}, {"date": ["21", "oct.", "2014", "18", "30"], "texte": " S\u00e9ance en rab' pour la nouvelle formation arduino (niveau 1)", "lieu": "Arduino 3.4 - La Ruche Num\u00e9rique"}];
-var events = past_events + future_events;
