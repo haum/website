dad06a2938daa163be965c0b9b40043057b90cc3
JackDesBwa <jackdesbwa@gmail.com>
2021-04-07 01:26:50 +0200

    Fix CATEGORY_FEED_ATOM warning

--- publishconf.py
+++ publishconf.py
@@ -15,7 +15,7 @@ SITEURL = ''
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 FEED_MAX_ITEMS = 10
 
 DELETE_OUTPUT_DIRECTORY = True
